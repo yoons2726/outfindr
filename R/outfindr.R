19e1b21abaf22fb6dd536281cf3e348bf121456f
wendy-shue <wendy.shue@mail.utoronto.ca>
2025-11-11 16:57:18 -0500

    initial commit

---
+++ R/outfindr.R
@@ -0,0 +1,6 @@
+outfindr <- function(data, method, range=NULL) {
+  if(method=="IQR"){print()}
+  if(method=="range"){print()}
+  if(method=="k-nearest"){print()}
+  else{print("please specify method");stop()}
+}
