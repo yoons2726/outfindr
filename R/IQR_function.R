e16ed9dcca8e94afe43ba790e41806541fbdc90b
Soohyun <yoons2726@gmail.com>
2025-11-22 16:37:20 -0500

    Add variable in IQR function

--- R/IQR_function.R
+++ R/IQR_function.R
@@ -1,10 +1,12 @@
 ## function calculating iqr range
-iqr_outlier_flags <- function(x, na.rm = TRUE, coef = 1.5) {
+iqr_outlier_flags <- function(data, var, na.rm = TRUE, coef = 1.5) {
+  x <- data[,var]
   q1  <- quantile(x, 0.25, na.rm = na.rm)
   q3  <- quantile(x, 0.75, na.rm = na.rm)
   iqr <- q3 - q1
   lower <- q1 - coef * iqr
   upper <- q3 + coef * iqr
+
+  # logical vector: TRUE = outlier
   x < lower | x > upper
 }
-
